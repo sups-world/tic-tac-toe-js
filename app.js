84dd06d7191b9b465412efa8a7bc07bf3fcfb6af
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-03 11:22:52 +0545

    wrote codes for checking win status

--- app.js
+++ app.js
@@ -22,10 +22,50 @@ function addGo(e) {
   //   console.log(e.target);
   const goDisplay = document.createElement("div");
   goDisplay.classList.add(go);
-  console.log("vlaue of go::", go);
   e.target.append(goDisplay);
   // changing turns here
   go = go == "circle" ? "cross" : "circle";
-  infoDisplay.textContent = "it is now" + go + "'s turn";
+  infoDisplay.textContent = "it is now " + go + "'s turn";
   e.target.removeEventListener("click", addGo);
+  checkScore();
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  winningCombos.forEach((array) => {
+    let circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+    if (circleWins) {
+      infoDisplay.textContent = "circle wins!!!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    let crossWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("cross")
+    );
+    if (crossWins) {
+      infoDisplay.textContent = "cross wins!!!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
 }
