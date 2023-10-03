0482062a94cda4f663ae912003c150500cafbac5
Suprajna Sharma <suprasubedi11@gmail.com>
2023-10-03 11:07:07 +0545

    added cross css, removeEventListener used,alternate turns for cross and circle completed

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const infoDisplay = document.querySelector("#info");
 
 const startCells = ["", "", "", "", "", "", "", "", ""];
 
+let go = "circle";
+infoDisplay.textContent = "Circle plays first";
+
 function createBoard() {
   startCells.forEach((_cell, index) => {
     const cellElement = document.createElement("div");
@@ -18,6 +21,11 @@ createBoard();
 function addGo(e) {
   //   console.log(e.target);
   const goDisplay = document.createElement("div");
-  goDisplay.classList.add("circle");
+  goDisplay.classList.add(go);
+  console.log("vlaue of go::", go);
   e.target.append(goDisplay);
+  // changing turns here
+  go = go == "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "it is now" + go + "'s turn";
+  e.target.removeEventListener("click", addGo);
 }
